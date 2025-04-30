8ae45f7745647d0f2318d60545e7bbc72589a468
kdelosangeles <kdelosangeles@testsys.com>
2025-04-30 09:57:48 -0400

    setup

---
+++ CommonScripts2/faq-accords.js
@@ -0,0 +1,78 @@
+// Expand All Accordions
+$("#ExpandAllAccrds").click(function () {
+  $(this).removeClass('shown').addClass('hidden');
+  $('#CollapseAllAccrds').removeClass('hidden').addClass('shown');
+
+  // Add the class to indicate all are expanded
+  $('.accordians').addClass('all-expanded');
+
+  // Add the 'open' class to all accordions and expand all panels
+  $('.accord').addClass('open').attr("aria-selected", "true");
+  $('.accord-panel').slideDown();
+  $('#CollapseAllAccrds').focus();
+});
+
+// Collapse All Accordions
+$("#CollapseAllAccrds").click(function () {
+  $(this).removeClass('shown').addClass('hidden');
+  $('#ExpandAllAccrds').removeClass('hidden').addClass('shown');
+
+  // Remove the class indicating all are expanded
+  $('.accordians').removeClass('all-expanded');
+
+  // Remove the 'open' class from all accordions and collapse all panels
+  $('.accord').removeClass('open').attr("aria-selected", "false");
+  $('.accord-panel').slideUp();
+  $('#ExpandAllAccrds').focus();
+
+  // expand all accordions for finder to search
+  $('#finder').removeClass('active');
+  $(finder.content).unhighlight();
+});
+
+// Individual Accordion Click
+$(".accord").on('click', function () {
+  // if accordion was open by #ExpandAllAccrds
+  if ($(".accordians").hasClass('all-expanded')) {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open').attr("aria-selected", "false");
+      $(this).next('.accord-panel').slideUp();
+    } else {
+      $(this).addClass('open').attr("aria-selected", "true");
+      $(this).next('.accord-panel').slideDown();
+    }
+  } else {
+    // normal behavior. only one accordion can be open at a time
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open').attr("aria-selected", "false");
+      $(this).next('.accord-panel').slideUp();
+    } else {
+      $('.accord').removeClass('open').attr("aria-selected", "false");
+      $('.accord-panel').slideUp();
+
+      $(this).addClass('open').attr("aria-selected", "true");
+      $(this).next('.accord-panel').slideDown();
+    }
+  }
+
+  // Check if all accordions are closed after this action
+  if ($('.accord.open').length === 0) {
+    $('#CollapseAllAccrds').removeClass('shown').addClass('hidden');
+    $('#ExpandAllAccrds').removeClass('hidden').addClass('shown');
+    $('.accordians').removeClass('all-expanded');
+  } else {
+    $('#ExpandAllAccrds').removeClass('shown').addClass('hidden');
+    $('#CollapseAllAccrds').removeClass('hidden').addClass('shown');
+    $('.accordians').removeClass('all-expanded');
+  }
+
+
+});
+
+// Handle keyboard interactions
+$(".accord").on('keydown', function (e) {
+  if ((e.key === "Enter" || e.key === " ") && $(this).is(":focus")) {
+    e.preventDefault(); // Prevent default spacebar scrolling
+    $(this).trigger('click');
+  }
+});
---
+++ CommonScripts2/jquery.finder.js
@@ -0,0 +1,207 @@
+
+/* jQuery Finder Plugin * @author Danny McGee * @version 1.0
+* github.com/dannymcgee * Copyright 2018 Danny McGee * Released under the MIT License
+* https://opensource.org/licenses/MIT */
+
+$(document).ready(function () {
+
+    $(finder.activator).click(function () {
+        $('.accord').addClass('open');
+        $('.accord-panel').slideDown();
+        $('#ExpandAllAccrds').addClass('hidden');
+        $('#CollapseAllAccrds').removeClass('hidden');
+        $('#CollapseAllAccrds').addClass('shown');
+        $('.accord').attr("aria-selected", "true");
+
+        $('#finderInput').removeAttr("aria-hidden");
+        $('#finderInput').attr("tabindex", "0");
+        $('#finderPrev').removeAttr("aria-hidden");
+        $('#finderPrev').attr("tabindex", "0");
+        $('#finderNext').removeAttr("aria-hidden");
+        $('#finderNext').attr("tabindex", "0");
+        $('#finderClose').removeAttr("aria-hidden");
+        $('#finderClose').attr("tabindex", "0");
+        finder.activate();
+    });
+
+
+    // $(document).mousedown(function (event) {  // old code
+    $(document).on("mousedown, mouseup, click", function (event) { //added click for a11y
+        if (event.which === 1) {
+            switch ($(event.target).attr('id') || $(event.target).parents().attr('id')) {
+                case 'finderClose':
+                    finder.closeFinder();
+                    // focus goes back to search button for a11y
+                    $('#OpenFinderSearcher').focus();
+                    break;
+                case 'finderPrev':
+                    finder.prevResult();
+                    break;
+                case 'finderNext':
+                    finder.nextResult();
+                    break;
+
+                default:
+                    return true;
+            }
+        }
+    });
+
+});
+
+const finder = {
+    activator: '[data-finder-activator]',
+
+    content: '[data-finder-content]',
+
+    wrapper: '[data-finder-wrapper]',
+
+    scrollOffset: () => $(finder.wrapper).data('finderScrollOffset'),
+
+    activate: () => {
+        if (!$('#finder').length) {
+            finder.createFinder();
+        }
+        setTimeout(function () {
+            $('#finder').addClass('findon');
+            $('#finderInput').focus();
+            if ($('#finderInput').val()) {
+                finder.findTerm($('#finderInput').val());
+            }
+            $('#finderInput').on('input', function () {
+                finder.findTerm($(this).val());
+            });
+        }, 50);
+    },
+
+    closeFinder: () => {
+        $('#finderInput').attr("aria-hidden", "true");
+        $('#finderInput').attr("tabindex", "-1");
+        $('#finderPrev').attr("aria-hidden", "true");
+        $('#finderPrev').attr("tabindex", "-1");
+        $('#finderNext').attr("aria-hidden", "true");
+        $('#finderNext').attr("tabindex", "-1");
+        $('#finderClose').attr("aria-hidden", "true");
+        $('#finderClose').attr("tabindex", "-1");
+
+        $('#finder').removeClass('findon');
+        $(finder.content).unhighlight();
+
+        // Clear the search input field
+        $('#finderInput').val('').removeClass('not-found');
+
+        // Remove the results count and related elements
+        $('.searchResult.visuallyhidden').remove();
+        $('#finderCount').remove();
+
+        // Reset the results tracking
+        finder.resultsCount = 0;
+        finder.currentResult = 0;
+
+        // Scroll the finder wrapper back to the top
+        $(finder.wrapper).scrollTop(0);
+    },
+
+    resultsCount: 0,
+
+    currentResult: 0,
+
+    findTerm: (term) => {
+        // highlight results
+        $(finder.content).unhighlight();
+        // Highlight only visible elements containing the term
+        $(finder.content).find(':visible').highlight(term);
+
+        // Count only visible results
+        finder.resultsCount = $('.findhighlight:visible').length;
+
+        if (finder.resultsCount) {
+            // there are results, scroll to first one
+            finder.currentResult = 1;
+            finder.scrollToCurrent();
+        } else {
+            // no results
+            finder.currentResult = 0;
+        }
+
+        // term not found
+        if (!finder.resultsCount && term) {
+            $('#finderInput').addClass('not-found');
+        } else {
+            $('#finderInput').removeClass('not-found');
+        }
+
+        finder.updateCurrent();
+    },
+
+    scrollToCurrent: () => {
+        let scrollingElement;
+
+        let i = finder.currentResult - 1;
+        $('.findhighlight').removeClass('findon');
+        const currentElement = $(`.findhighlight:eq(${i})`);
+        currentElement.addClass('findon');
+
+        let offsetTop = -100;
+        if (finder.scrollOffset() !== null) {
+            offsetTop = finder.scrollOffset() * -1;
+        }
+
+        $(finder.wrapper).scrollTo('.findhighlight.findon', {
+            offset: {
+                left: 0,
+                top: offsetTop,
+            },
+        });
+    },
+
+    prevResult: () => {
+        if (finder.resultsCount) {
+            if (finder.currentResult > 1) {
+                finder.currentResult--;
+            } else {
+                finder.currentResult = finder.resultsCount;
+            }
+            finder.scrollToCurrent();
+        }
+        finder.updateCurrent();
+    },
+
+    nextResult: () => {
+        if (finder.resultsCount) {
+            if (finder.currentResult < finder.resultsCount) {
+                finder.currentResult++;
+            } else {
+                finder.currentResult = 1;
+            }
+            finder.scrollToCurrent();
+        }
+        finder.updateCurrent();
+    },
+
+    updateCurrent: () => {
+        if ($('#finderInput').val()) {
+            if (!$('#finderCount').length) {
+                const countElem = $('<span />')
+                    .attr({
+                        'id': 'finderCount',
+                        'class': 'finder-count',
+                    })
+                    .insertAfter('#finderInput');
+                // create element with that will read search result
+                $('#finderCount').after("<span class='searchResult visuallyhidden' aria-live='polite'></span>");
+
+            }
+            setTimeout(function () {
+                $('#finderCount').text(finder.currentResult + ' / ' + finder.resultsCount);
+                // new for A11Y
+                $('.searchResult').text("Search Result " + finder.currentResult + ' of ' + finder.resultsCount); // say the result term
+                $('.findhighlight.findon').attr({ 'aria-live': 'polite' }); // say the paragraph that contain the search term
+            }, 500);
+        } else {
+            $('.searchResult.visuallyhidden').remove();
+            $('#finderCount').remove();
+        }
+    },
+}
+
